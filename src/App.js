8c50c61f8d6a2efa14042787fcfdda51877d68b0
Abhi Pillai <abhi.pillai1@gmail.com>
2016-11-06 17:47:20 -0500

    Setup a generic passage input form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 const logo = '//d2t498vi8pate3.cloudfront.net/assets/home-header-logo-8d37f4195730352f0055d39f7e88df602e2d67bdab1000ac5886c5a492400c9d.png';
 import './App.css';
 
+import PassageInputForm from './components/PassageInputForm'
+
 class App extends Component {
   render() {
     return (
@@ -10,11 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to the Quill Web Dev Challenge</h2>
         </div>
-        <p className="App-intro">
-          Refer to the <a href="https://github.com/empirical-org/quill-web-dev-challenge" target="_blank">README</a> for the instructions to this challenge.
-          <br/>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PassageInputForm />
       </div>
     );
   }
---
+++ src/components/PassageInputForm.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PassageInputForm extends Component {
+  render () {
+    return (
+      <div>
+        <textarea />
+        <button>Next</button>
+      </div>
+    )
+  }
+}
+
+export default PassageInputForm;
