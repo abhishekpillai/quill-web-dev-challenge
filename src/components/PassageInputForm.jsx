0e79cdc380975a9c720cd425dcb78d0138cfd5a9
Abhi Pillai <abhi.pillai1@gmail.com>
2016-11-06 17:54:50 -0500

    Turn passage input form into controlled form

--- src/components/PassageInputForm.jsx
+++ src/components/PassageInputForm.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class PassageInputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {passage: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({passage: event.target.value});
+  }
+
+  handleNext(event) {
+    alert('Text field value is: ' + this.state.passage);
+  }
+
   render () {
     return (
       <div>
-        <textarea />
-        <button>Next</button>
+        <textarea onChange={this.handleChange} />
+
+        <button onClick={this.handleNext} >
+          Next
+        </button>
       </div>
     )
   }
